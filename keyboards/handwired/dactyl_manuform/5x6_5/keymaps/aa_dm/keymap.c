98bb2ef8f69325b957beab4771c6d9d76242a2dd
Aaron <aaronwixner@gmail.com>
2022-11-13 21:04:50 -0700

    better RGB key codes

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/aa_dm/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/aa_dm/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 				
 		[LAYER_NUMS] = LAYOUT_5x6_5(		
-		KC_NO, 		KC_NO, 				TO(0), 				TO(1), 				KC_NO,		KC_NO, 													KC_NO, 			KC_LEFT, 		KC_UP, 				KC_DOWN, 			KC_RGHT, 				KC_NO, 
+		RGB_VAI, 	RGB_VAD, 			TO(0), 				TO(1), 				KC_NO,		KC_NO, 													KC_NO, 			KC_LEFT, 		KC_UP, 				KC_DOWN, 			KC_RGHT, 				KC_NO, 
 		KC_F1, 		KC_F2, 				KC_F3, 				KC_F4, 				KC_NO, 		KC_NO, 													KC_NO, 			KC_P7, 			KC_P8, 				KC_P9, 				KC_NUM, 				KC_NO, 
 		KC_F5, 		KC_F6, 				KC_F7, 				KC_F8, 				KC_NO, 		KC_NO, 													KC_NO, 			KC_P4, 			KC_P5, 				KC_P6, 				KC_DOT, 				KC_NO, 
 		KC_F9, 		KC_F10, 			KC_F11, 			KC_F12, 			KC_NO, 		KC_NO, 													KC_NO, 			KC_P1, 			KC_P2, 				KC_P3, 				KC_P0, 					KC_NO, 
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			
 			
 		[LAYER_MATHSYMS] = LAYOUT_5x6_5(	
-		KC_NO, 		KC_NO, 		KC_NO, 				KC_NO, 				TO(LAYER_QWERTY), 	KC_NO, 															KC_NO, 			KC_LEFT, 	KC_UP, 				KC_DOWN, 			KC_RGHT, 	KC_NO, 	
+		RGB_MOD,	RGB_TOG, 	KC_NO, 				KC_NO, 				TO(LAYER_QWERTY), 	KC_NO, 															KC_NO, 			KC_LEFT, 	KC_UP, 				KC_DOWN, 			KC_RGHT, 	KC_NO, 	
 		KC_COMM, 	KC_PERC, 	KC_AT, 				KC_QUOT, 			KC_COLN, 			KC_NO, 															KC_NO, 			KC_0, 		KC_CIRC, 			KC_DLR, 			KC_NO, 		KC_NO, 
 		KC_SLSH, 	KC_LCBR, 	KC_SCLN, 			KC_LPRN, 			KC_PLUS, 			KC_NO, 															KC_ASTR, 		KC_RPRN, 	KC_EQL, 			KC_RCBR, 			KC_MINS, 	KC_NO, 
 		KC_HASH, 	KC_GRV, 	KC_AMPR, 			KC_QUES, 			KC_PIPE, 			KC_NO, 															KC_BSLS, 		KC_EXLM, 	KC_LBRC, 			KC_RBRC, 			KC_UNDS, 	KC_NO,
